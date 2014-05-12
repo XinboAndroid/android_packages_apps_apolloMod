ed5baea1ece6a6d0327e17724e365c9a77600f09
XinboAndroid <suiyc@xinboedu.com>
2014-05-13 00:29:40 +0800

    （R）歌曲列表弹框提示

--- src/com/andrew/apolloMod/menu/PlaylistPicker.java
+++ src/com/andrew/apolloMod/menu/PlaylistPicker.java
@@ -1,4 +1,3 @@
-
 package com.andrew.apolloMod.menu;
 
 import java.util.ArrayList;
@@ -22,8 +21,9 @@ import static com.andrew.apolloMod.Constants.INTENT_PLAYLIST_LIST;
 import static com.andrew.apolloMod.Constants.PLAYLIST_NEW;
 import static com.andrew.apolloMod.Constants.PLAYLIST_QUEUE;
 
-public class PlaylistPicker extends Activity implements DialogInterface.OnClickListener,
-        DialogInterface.OnCancelListener {
+public class PlaylistPicker extends Activity implements
+        DialogInterface.OnClickListener, DialogInterface.OnCancelListener
+{
 
     private AlertDialog mPlayListPickerDialog;
 
@@ -31,22 +31,28 @@ public class PlaylistPicker extends Activity implements DialogInterface.OnClickL
 
     List<String> mPlayListNames = new ArrayList<String>();
 
-    long[] mList = new long[] {};
+    long[] mList = new long[]
+    {};
 
     @Override
-    public void onCancel(DialogInterface dialog) {
+    public void onCancel(DialogInterface dialog)
+    {
         finish();
     }
 
+    // onClick()?
     @Override
-    public void onClick(DialogInterface dialog, int which) {
+    public void onClick(DialogInterface dialog, int which)
+    {
 
         long listId = Long.valueOf(mAllPlayLists.get(which).get("id"));
         String listName = mAllPlayLists.get(which).get("name");
         Intent intent;
-        boolean mCreateShortcut = getIntent().getAction().equals(Intent.ACTION_CREATE_SHORTCUT);
-
-        if (mCreateShortcut) {
+        boolean mCreateShortcut = getIntent().getAction().equals(
+                Intent.ACTION_CREATE_SHORTCUT);
+        //创建快捷方式
+        if (mCreateShortcut)
+        {
             final Intent shortcut = new Intent();
             // shortcut.setAction(INTENT_PLAY_SHORTCUT);
             shortcut.putExtra("id", listId);
@@ -55,14 +61,26 @@ public class PlaylistPicker extends Activity implements DialogInterface.OnClickL
             intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcut);
             intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, listName);
             intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
-                    Intent.ShortcutIconResource.fromContext(this, R.drawable.ic_launcher));
+                    Intent.ShortcutIconResource.fromContext(this,
+                            R.drawable.ic_launcher));
+            //TracksBrowser
             setResult(RESULT_OK, intent);
-        } else {
-            if (listId >= 0) {
+        }
+        else
+        {
+            //存在艺术家列表ID
+            if (listId >= 0)
+            {
                 MusicUtils.addToPlaylist(this, mList, listId);
-            } else if (listId == PLAYLIST_QUEUE) {
+            }
+            //添加艺术家到队列
+            else if (listId == PLAYLIST_QUEUE)
+            {
                 MusicUtils.addToCurrentPlaylist(this, mList);
-            } else if (listId == PLAYLIST_NEW) {
+            }
+            //新建列表名
+            else if (listId == PLAYLIST_NEW)
+            {
                 intent = new Intent(INTENT_CREATE_PLAYLIST);
                 intent.putExtra(INTENT_PLAYLIST_LIST, mList);
                 startActivity(intent);
@@ -72,54 +90,82 @@ public class PlaylistPicker extends Activity implements DialogInterface.OnClickL
     }
 
     @Override
-    public void onCreate(Bundle icicle) {
+    public void onCreate(Bundle icicle)
+    {
 
         super.onCreate(icicle);
         setContentView(new LinearLayout(this));
 
-        if (getIntent().getAction() != null) {
-
+        if (getIntent().getAction() != null)
+        {
+            // 添加歌曲
             if (INTENT_ADD_TO_PLAYLIST.equals(getIntent().getAction())
-                    && getIntent().getLongArrayExtra(INTENT_PLAYLIST_LIST) != null) {
-
+                    && getIntent().getLongArrayExtra(INTENT_PLAYLIST_LIST) != null)
+            {
+                //
                 MusicUtils.makePlaylistList(this, false, mAllPlayLists);
                 mList = getIntent().getLongArrayExtra(INTENT_PLAYLIST_LIST);
-                for (int i = 0; i < mAllPlayLists.size(); i++) {
-                    mPlayListNames.add(mAllPlayLists.get(i).get("name"));
-                }
-                mPlayListPickerDialog = new AlertDialog.Builder(this)
-                        .setTitle(R.string.add_to_playlist)
-                        .setItems(mPlayListNames.toArray(new CharSequence[mPlayListNames.size()]),
-                                this).setOnCancelListener(this).show();
-            } else if (getIntent().getAction().equals(Intent.ACTION_CREATE_SHORTCUT)) {
-                MusicUtils.makePlaylistList(this, true, mAllPlayLists);
-                for (int i = 0; i < mAllPlayLists.size(); i++) {
-                    mPlayListNames.add(mAllPlayLists.get(i).get("name"));
-                }
-                mPlayListPickerDialog = new AlertDialog.Builder(this)
-                        .setItems(mPlayListNames.toArray(new CharSequence[mPlayListNames.size()]),
-                                this).setOnCancelListener(this).show();
+                //封装
+                addToPlay(R.string.add_to_playlist);
             }
-        } else {
+            //创建快捷方式
+            else if (getIntent().getAction().equals(
+                    Intent.ACTION_CREATE_SHORTCUT))
+            {
+                MusicUtils.makePlaylistList(this, true, mAllPlayLists);
+
+                addToPlay(R.string.create_Shortcut);
+//                for (int i = 0; i < mAllPlayLists.size(); i++)
+//                {
+//                    mPlayListNames.add(mAllPlayLists.get(i).get("name"));
+//                }
+//                mPlayListPickerDialog = new AlertDialog.Builder(this)
+//                .setTitle(R.string.create_Shortcut)
+//                        .setItems(
+//                                mPlayListNames.toArray(new CharSequence[mPlayListNames
+//                                        .size()]), this)
+//                        .setOnCancelListener(this).show();
+            }
+        }
+        else
+        {
             Toast.makeText(this, R.string.error, Toast.LENGTH_SHORT).show();
             finish();
         }
     }
 
-    @Override
-    public void onPause() {
+    private void addToPlay(int title)
+    {
+        for (int i = 0; i < mAllPlayLists.size(); i++)
+        {
+            mPlayListNames.add(mAllPlayLists.get(i).get("name"));
+        }
+        mPlayListPickerDialog = new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setItems(
+                        mPlayListNames.toArray(new CharSequence[mPlayListNames
+                                .size()]), this)
+                .setOnCancelListener(this).show();
+    }
 
-        if (mPlayListPickerDialog != null && mPlayListPickerDialog.isShowing()) {
+    @Override
+    public void onPause()
+    {
+
+        if (mPlayListPickerDialog != null && mPlayListPickerDialog.isShowing())
+        {
             mPlayListPickerDialog.dismiss();
         }
         super.onPause();
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
 
         super.onResume();
-        if (mPlayListPickerDialog != null && !mPlayListPickerDialog.isShowing()) {
+        if (mPlayListPickerDialog != null && !mPlayListPickerDialog.isShowing())
+        {
             mPlayListPickerDialog.show();
         }
     }
